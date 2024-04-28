c5d4e1732e1c1585e4f5576216af4d23a399ac07
UjjawalGusain <ujjawalgusain31@gmail.com>
2024-04-28 17:57:47 +0530

    Add WalletSection component

---
+++ src/components/Dashboard/WalletTab/WalletComponents/WalletCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BsSuitcaseLg } from "react-icons/bs";
+import { CiWallet } from "react-icons/ci";
+import { IoPricetagsOutline } from "react-icons/io5";
+
+function WalletCard({icon, balance}) {
+
+   let selectedIcon;
+   switch (icon) {
+     case 'Portfolio Amount':
+       selectedIcon = <BsSuitcaseLg />;
+       break;
+     case 'Wallet Amount':
+       selectedIcon = <CiWallet />;
+       break;
+     case 'Credit':
+       selectedIcon = <IoPricetagsOutline />;
+       break;
+     default:
+       selectedIcon = null; 
+   }
+
+
+   return (
+    <div className='flex flex-col border-black border'>
+      <div className='flex '>
+        <div>{selectedIcon}</div>
+        <div>{icon}</div>
+      </div>
+      <div>{balance}</div>
+    </div>
+  );
+}
+
+export default WalletCard
--- src/components/Dashboard/WalletTab/WalletSection.jsx
+++ src/components/Dashboard/WalletTab/WalletSection.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import WalletCard from './WalletComponents/WalletCard'
+import { useSelector } from 'react-redux'
 
 const WalletSection = () => {
+  const { user } = useSelector((state) => state.profile);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>WallletSection</div>
+    <>
+      <div className='flex gap-2'>
+        <WalletCard icon="Portfolio Amount" balance={user.portfolioBalance}/>
+        <WalletCard icon="Wallet Amount" balance={user.walletBalance}/>
+        <WalletCard icon="Credit" balance="69"/>
+      </div>
+    </>
   )
 }
 
