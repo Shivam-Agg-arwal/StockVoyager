d841f8bc3b552b037f9004d0386e3693f88c1871
Shivam Aggarwal <bullseyeanalyst@gmail.com>
2024-04-28 11:14:50 +0530

    indentation

--- src/components/Dashboard/Settings/UpdateDP.jsx
+++ src/components/Dashboard/Settings/UpdateDP.jsx
@@ -1,21 +1,20 @@
-import React, { useState } from 'react'
-import { WiDayCloudy } from 'react-icons/wi';
-import { useDispatch, useSelector } from 'react-redux'
-import toast from 'react-hot-toast';
-import axios from 'axios';
-import { updationEndpoints } from '../../../api/api';
-import { setUser } from '../../slices/profileSlice';
+import React, { useState } from "react";
+import { WiDayCloudy } from "react-icons/wi";
+import { useDispatch, useSelector } from "react-redux";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { updationEndpoints } from "../../../api/api";
+import { setUser } from "../../slices/profileSlice";
 
 const UpdateDP = () => {
-    const {user}=useSelector((state)=>state.profile);
-    const dispatch=useDispatch();
-    const [chosenImage,setChosenImage]=useState(null);
-    const [file,setFile]=useState(null);
+    const { user } = useSelector((state) => state.profile);
+    const dispatch = useDispatch();
+    const [chosenImage, setChosenImage] = useState(null);
+    const [file, setFile] = useState(null);
 
-    const {UPDATE_DP_API}=updationEndpoints;
+    const { UPDATE_DP_API } = updationEndpoints;
 
-
-    const {token}=useSelector((state)=>state.auth);
+    const { token } = useSelector((state) => state.auth);
     const handleFileInputChange = (event) => {
         const file = event.target.files[0];
         if (file) {
@@ -28,57 +27,88 @@ const UpdateDP = () => {
             reader.readAsDataURL(file);
         }
     };
-    const handleUpdation=async()=>{
-        const loadingToast=toast.loading('Updating Display Picture...');
+    const handleUpdation = async () => {
+        const loadingToast = toast.loading("Updating Display Picture...");
         try {
-			
-			const formData = new FormData();
-			formData.append('token', token);
-			formData.append('displayPicture', file);
+            const formData = new FormData();
+            formData.append("token", token);
+            formData.append("displayPicture", file);
             // console.log('calling');
 
-			const response=await axios.post(UPDATE_DP_API,formData);
-			console.log(response);
-			if(response.data.success){
-				toast.success('Updated successfully');
-				dispatch(setUser(response.data.data));
-				localStorage.setItem("user", JSON.stringify(response.data.data));
+            const response = await axios.post(UPDATE_DP_API, formData);
+            console.log(response);
+            if (response.data.success) {
+                toast.success("Updated successfully");
+                dispatch(setUser(response.data.data));
+                localStorage.setItem(
+                    "user",
+                    JSON.stringify(response.data.data)
+                );
                 RemoveHandle();
-			}
-			
-
-		} catch (error) {
-			toast.error('Updation failed');
+            }
+        } catch (error) {
+            toast.error("Updation failed");
             console.log("error ");
-			
-		}
+        }
         toast.dismiss(loadingToast);
-    }
+    };
 
-    const RemoveHandle=()=>{
+    const RemoveHandle = () => {
         setFile(null);
         setChosenImage(null);
-    }
+    };
 
-
-
-
-  return (
-    <div className='flex flex-row gap-2 items-center'>
-        <img src={`${chosenImage?chosenImage:user.image}`} width={90} className='rounded-full'/>
-        <div>
+    return (
+        <div className="flex flex-row gap-2 items-center">
+            <img
+                src={`${chosenImage ? chosenImage : user.image}`}
+                width={90}
+                className="rounded-full"
+            />
             <div>
-                Change Profile Picture
-            </div>
-            <div className='flex flex-row gap-2'>
-                {file && <div onClick={()=>{handleUpdation()}} className='cursor-pointer'>Update Image</div>}
-                {!file && <div><label htmlFor='uploadimage' className='cursor-pointer'>Choose Image</label>
-                <input type="file" id='uploadimage' className='hidden' onChange={handleFileInputChange} accept="image/*"/></div>}
-                {file && <button onClick={()=>{RemoveHandle()}} className='cursor-pointer'>Remove</button>}
+                <div>Change Profile Picture</div>
+                <div className="flex flex-row gap-2">
+                    {file && (
+                        <div
+                            onClick={() => {
+                                handleUpdation();
+                            }}
+                            className="cursor-pointer"
+                        >
+                            Update Image
+                        </div>
+                    )}
+                    {!file && (
+                        <div>
+                            <label
+                                htmlFor="uploadimage"
+                                className="cursor-pointer"
+                            >
+                                Choose Image
+                            </label>
+                            <input
+                                type="file"
+                                id="uploadimage"
+                                className="hidden"
+                                onChange={handleFileInputChange}
+                                accept="image/*"
+                            />
+                        </div>
+                    )}
+                    {file && (
+                        <button
+                            onClick={() => {
+                                RemoveHandle();
+                            }}
+                            className="cursor-pointer"
+                        >
+                            Remove
+                        </button>
+                    )}
+                </div>
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default UpdateDP
+export default UpdateDP;
