8d8afaea93f76e7a9d6636681e4cdc3c050328f7
Shivam Aggarwal <bullseyeanalyst@gmail.com>
2024-05-03 20:11:55 +0530

    sell stock api completed

--- server/routes/Stock.js
+++ server/routes/Stock.js
@@ -1,10 +1,11 @@
 const express=require('express');
 const router=express.Router();
 
-const {buyStock}=require('../controllers/Stock');
+const {buyStock,sellStock}=require('../controllers/Stock');
 
 const{auth}=require('../middlewares/auth');
 
 router.post('/buyStock',auth,buyStock);
+router.post('/sellStock',auth,sellStock);
 
 module.exports=router;
