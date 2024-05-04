4795d1cfacf13cdfe9400d420da019f9711e7009
Shivam Aggarwal <bullseyeanalyst@gmail.com>
2024-05-04 17:35:08 +0530

    fdas

--- src/components/Dashboard/Portfolio.jsx
+++ src/components/Dashboard/Portfolio.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ApexChart from './WalletTab/Graphical'
+import Loader from '../Loader'
 
 const Portfolio = () => {
   return (
     <h1 className='text-5xl w-screen h-screen flex justify-center items-center'>
     <ApexChart/>
+    <Loader/>
     </h1>
   )
 }
---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+const Loader = () => {
+    return (
+        <div className='loader'>
+            
+        </div>
+    )
+}
+
+export default Loader
