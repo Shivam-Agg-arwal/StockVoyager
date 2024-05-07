7d8e9eeb41c9e6a8575f8348af6362f91ab0a1ca
UjjawalGusain <ujjawalgusain31@gmail.com>
2024-05-07 05:34:18 +0530

    Added reali-time reading in Portfolio Graph

--- src/components/Dashboard/Portfolio/Graph.jsx
+++ src/components/Dashboard/Portfolio/Graph.jsx
@@ -1,82 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactApexChart from 'react-apexcharts';
+import { connect } from 'react-redux';
+import fetchCurrentPrice from '../../../../pyserver/MakeRequest/getStockCurrentPrice';
 
-class ApexChart extends React.Component {
+class ApexChart extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            series: [{
-                name: 'Bought Value',
-                data: [
-                    [1327359600000, 30.95],
-                    [1327446000000, 31.34],
-                    [1327532400000, 31.18],
-                    [1327618800000, 31.05],
-                    [1327878000000, 31.00],
-                    [1327964400000, 30.95],
-                    [1328050800000, 31.24],
-                    [1328137200000, 31.29],
-                    [1328223600000, 31.85],
-                    [1328482800000, 31.86],
-                    [1328569200000, 32.28],
-                    [1328655600000, 32.10],
-                    [1328742000000, 32.65],
-                    [1328828400000, 32.21],
-                    [1329087600000, 32.35],
-                    [1329174000000, 32.44],
-                    [1329260400000, 32.46],
-                    [1329346800000, 32.86],
-                    [1329433200000, 32.75],
-                    [1329778800000, 32.54],
-                    [1329865200000, 32.33],
-                    [1329951600000, 32.97],
-                    [1330038000000, 33.41],
-                    [1330297200000, 33.27],
-                    [1330383600000, 33.27],
-                    [1330470000000, 32.89],
-                    [1330556400000, 33.10],
-                    [1330642800000, 33.73],
-                    [1330902000000, 33.22],
-                    [1330988400000, 31.99],
-                    // Add more data points for bought value as needed
-                ]
-            }, {
-                name: 'Current Value',
-                data: [
-                    [1327359600000, 32.05],
-                    [1327446000000, 32.50],
-                    [1327532400000, 32.40],
-                    [1327618800000, 32.30],
-                    [1327878000000, 32.20],
-                    [1327964400000, 32.15],
-                    [1328050800000, 32.24],
-                    [1328137200000, 32.19],
-                    [1328223600000, 32.25],
-                    [1328482800000, 32.26],
-                    [1328569200000, 32.68],
-                    [1328655600000, 32.50],
-                    [1328742000000, 32.95],
-                    [1328828400000, 32.51],
-                    [1329087600000, 32.65],
-                    [1329174000000, 32.74],
-                    [1329260400000, 32.76],
-                    [1329346800000, 33.16],
-                    [1329433200000, 33.05],
-                    [1329778800000, 32.84],
-                    [1329865200000, 32.63],
-                    [1329951600000, 33.27],
-                    [1330038000000, 33.71],
-                    [1330297200000, 33.57],
-                    [1330383600000, 33.57],
-                    [1330470000000, 33.19],
-                    [1330556400000, 33.40],
-                    [1330642800000, 34.03],
-                    [1330902000000, 33.52],
-                    [1330988400000, 32.29],
-                    // Add more data points for current value as needed
-                ]
-            }],
+            series: [
+                {
+                    name: 'Bought Value',
+                    data: []
+                },
+                {
+                    name: 'Current Value',
+                    data: []
+                }
+            ],
             options: {
                 chart: {
                     id: 'area-datetime',
@@ -112,26 +53,118 @@ class ApexChart extends React.Component {
                 },
             },
             selection: 'one_year',
+            error: null,
+            priceData: {} // Store fetched price data
         };
     }
 
-    updateData(timeline) {
-        this.setState({
-            selection: timeline
-        })
+    componentDidMount() {
+        this.fetchChartData();
+    }
 
-        // Implement the logic to update data based on timeline selection
+    componentDidUpdate(prevProps) {
+        if (prevProps.user !== this.props.user) {
+            this.fetchChartData();
+        }
+    }
+
+    fetchChartData = async () => {
+        try {
+            const { user } = this.props;
+
+            if (!user || !user.transactions) {
+                throw new Error("User data or transactions not available");
+            }
+
+            const { transactions } = user;
+
+            const boughtValueData = this.calculateBoughtValueOverTime(transactions);
+            const currentValueData = await this.calculateCurrentValueOverTime(transactions, boughtValueData);
+
+            this.setState({
+                series: [
+                    { ...this.state.series[0], data: boughtValueData },
+                    { ...this.state.series[1], data: currentValueData }
+                ],
+                error: null
+            });
+        } catch (error) {
+            console.error('Error:', error);
+            this.setState({ error: error.message });
+        }
+    }
+
+    calculateBoughtValueOverTime = (transactions) => {
+        const boughtValues = [];
+        let totalBoughtValue = 0;
+
+        for (const transaction of transactions) {
+            const timestamp = new Date(transaction.orderDate).getTime();
+            if (transaction.orderType === "Buy") {
+                totalBoughtValue += transaction.tradeValue;
+            } else if (transaction.orderType === "Sell") {
+                totalBoughtValue -= transaction.tradeValue;
+            }
+            boughtValues.push([timestamp, totalBoughtValue]);
+        }
+
+        return boughtValues;
+    }
+
+    calculateCurrentValueOverTime = async (transactions, boughtValues) => {
+        const currentValueData = [];
+
+        for (const [timestamp, boughtValue] of boughtValues) {
+            let totalCurrentValue = 0;
+
+            for (const transaction of transactions) {
+                if (new Date(transaction.orderDate).getTime() <= timestamp) {
+                    // Check if price data is already fetched for this stock symbol
+                    const { stockSymbol } = transaction;
+                    let currentPrice = this.state.priceData[stockSymbol];
+
+                    // If price data is not already fetched, fetch it and store in memory
+                    if (!currentPrice) {
+                        const currentPriceData = await fetchCurrentPrice(stockSymbol);
+                        currentPrice = currentPriceData.lastPrice;
+                        this.setState(prevState => ({
+                            priceData: {
+                                ...prevState.priceData,
+                                [stockSymbol]: currentPrice
+                            }
+                        }));
+                    }
+
+                    totalCurrentValue += currentPrice * transaction.quantity;
+                }
+            }
+
+            currentValueData.push([timestamp, totalCurrentValue]);
+        }
+
+        return currentValueData;
     }
 
     render() {
+        const { error } = this.state;
+
+        if (error) {
+            return <div>Error: {error}</div>;
+        }
+
         return (
             <div>
-                <div id="chart">
+                <h2 className="text-2xl text-center font-bold">Portfolio Performance</h2>
+                <div className="chart-container" style={{ width: "700px", height: "550px", margin: "auto" }}>
                     <ReactApexChart options={this.state.options} series={this.state.series} type="area" height={350} />
                 </div>
             </div>
-        );
+        );        
     }
 }
 
-export default ApexChart;
+const mapStateToProps = (state) => ({
+    user: state.profile.user,
+});
+
+export default connect(mapStateToProps)(ApexChart);
--- src/components/Dashboard/Portfolio/PieChart.jsx
+++ src/components/Dashboard/Portfolio/PieChart.jsx
@@ -1,116 +1,130 @@
-import React, { Component } from 'react';
-import Chart from 'react-apexcharts';
-import industries from '../../../data/industries_data.json';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+import industries from "../../../data/industries_data.json";
+import { connect } from "react-redux";
 class Donut extends Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        options: {
-          chart: {
-            width: 700, // Adjust the width as needed
-            height: 600, // Adjust the height as needed
-            type: 'donut',
-          },
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {
+        chart: {
+          width: 700, // Adjust the width as needed
+          height: 600, // Adjust the height as needed
+          type: "donut",
         },
-        series: [],
-        labels: [],
-        portfolioData: [],
-        industryData: {},
-        dataProcessed: false // Flag to track if data processing is complete
-      };
-    }
-  
-    componentDidMount() {
+      },
+      series: [],
+      labels: [],
+      portfolioData: [],
+      industryData: {},
+      dataProcessed: false, // Flag to track if data processing is complete
+    };
+  }
+
+  componentDidMount() {
+    this.updatePortfolioData(this.props.user);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.user !== this.props.user) {
       this.updatePortfolioData(this.props.user);
     }
-  
-    componentDidUpdate(prevProps) {
-      if (prevProps.user !== this.props.user) {
-        this.updatePortfolioData(this.props.user);
-      }
+  }
+
+  updatePortfolioData = (user) => {
+    if (user && user.portfolio) {
+      const formattedData = user.portfolio.map((item) => {
+        const industry = industries.find(
+          (industry) => industry.symbol === item.stockSymbol
+        );
+        return {
+          ...item,
+          industryName: industry ? industry.industry : "",
+        };
+      });
+      this.setState(
+        { portfolioData: formattedData },
+        this.calculateIndustryData
+      );
     }
-  
-    updatePortfolioData = (user) => {
-      if (user && user.portfolio) {
-        const formattedData = user.portfolio.map((item) => {
-          const industry = industries.find((industry) => industry.symbol === item.stockSymbol);
-          return {
-            ...item,
-            industryName: industry ? industry.industry : ''
-          };
-        });
-        this.setState({ portfolioData: formattedData }, this.calculateIndustryData);
-      }
-    };
-  
-    calculateIndustryData = () => {
-      const { portfolioData } = this.state;
-      if (portfolioData.length > 0) {
-        const industryStats = portfolioData.reduce((acc, item) => {
-          const { industryName, quantity, buy_cost } = item;
-          if (!acc[industryName]) {
-            acc[industryName] = { totalQuantity: 0, totalBuyCost: 0 };
-          }
-          acc[industryName].totalQuantity += quantity;
-          acc[industryName].totalBuyCost += quantity * buy_cost;
-          return acc;
-        }, {});
-        this.setState({ industryData: industryStats }, this.calculatePieChartData);
-      }
-    };
-  
-    calculatePieChartData = () => {
-  const { industryData } = this.state;
-  const sortedIndustries = Object.entries(industryData).sort(([, a], [, b]) => b.totalBuyCost - a.totalBuyCost);
-  const topTenIndustries = sortedIndustries.slice(0, 10);
-  const otherIndustriesBuyCost = sortedIndustries.slice(10).reduce((total, [, { totalBuyCost }]) => total + totalBuyCost, 0);
-  const totalBuyCostAllIndustries = sortedIndustries.reduce((total, [, { totalBuyCost }]) => total + totalBuyCost, 0);
-  const topTenPercentages = topTenIndustries.map(([industry, { totalBuyCost }]) => ({
-    industry,
-    percentage: (totalBuyCost / totalBuyCostAllIndustries) * 100
-  }));
-  const labels = [...topTenPercentages.map(({ industry }) => industry), 'Others'];
-  const series = [
-    ...topTenPercentages.map(({ percentage }) => percentage),
-    (otherIndustriesBuyCost / totalBuyCostAllIndustries) * 100
-  ];
-  this.setState({ labels, series, dataProcessed: true });
-  
-  // Update the labels in the options state
-  this.setState(prevState => ({
-    options: {
-      ...prevState.options,
-      labels: labels
+  };
+
+  calculateIndustryData = () => {
+    const { portfolioData } = this.state;
+    if (portfolioData.length > 0) {
+      const industryStats = portfolioData.reduce((acc, item) => {
+        const { industryName, quantity, buy_cost } = item;
+        if (!acc[industryName]) {
+          acc[industryName] = { totalQuantity: 0, totalBuyCost: 0 };
+        }
+        acc[industryName].totalQuantity += quantity;
+        acc[industryName].totalBuyCost += quantity * buy_cost;
+        return acc;
+      }, {});
+      this.setState(
+        { industryData: industryStats },
+        this.calculatePieChartData
+      );
     }
-  }));
-};
+  };
 
+  calculatePieChartData = () => {
+    const { industryData } = this.state;
+    const sortedIndustries = Object.entries(industryData).sort(
+      ([, a], [, b]) => b.totalBuyCost - a.totalBuyCost
+    );
+    const topTenIndustries = sortedIndustries.slice(0, 10);
+    const otherIndustriesBuyCost = sortedIndustries
+      .slice(10)
+      .reduce((total, [, { totalBuyCost }]) => total + totalBuyCost, 0);
+    const totalBuyCostAllIndustries = sortedIndustries.reduce(
+      (total, [, { totalBuyCost }]) => total + totalBuyCost,
+      0
+    );
+    const topTenPercentages = topTenIndustries.map(
+      ([industry, { totalBuyCost }]) => ({
+        industry,
+        percentage: (totalBuyCost / totalBuyCostAllIndustries) * 100,
+      })
+    );
+    const labels = [
+      ...topTenPercentages.map(({ industry }) => industry),
+      "Others",
+    ];
+    const series = [
+      ...topTenPercentages.map(({ percentage }) => percentage),
+      (otherIndustriesBuyCost / totalBuyCostAllIndustries) * 100,
+    ];
+    this.setState({ labels, series, dataProcessed: true });
 
-    
-  
+    // Update the labels in the options state
+    this.setState((prevState) => ({
+      options: {
+        ...prevState.options,
+        labels: labels,
+      },
+    }));
+  };
 
-
-render() {
+  render() {
     const { options, labels, series, dataProcessed } = this.state;
-  
+
     return (
       <div>
-        <h2 className='text-2xl text-center font-bold'>Industry Portfolio</h2>
-        <div className="donut" style={{ width: '600px', height: '550px' }}>
-          {dataProcessed && <Chart options={options} series={series} type="donut" />}
+        <h2 className="text-2xl text-center font-bold">Industry Portfolio</h2>
+        <div className="donut" style={{ width: "600px", height: "550px" }}>
+          {dataProcessed && (
+            <Chart options={options} series={series} type="donut" />
+          )}
         </div>
       </div>
     );
   }
-  
-  
 }
-  
-  const mapStateToProps = (state) => ({
-    user: state.profile.user
-  });
-  
-  export default connect(mapStateToProps)(Donut);
-  
+
+const mapStateToProps = (state) => ({
+  user: state.profile.user,
+});
+
+export default connect(mapStateToProps)(Donut);
